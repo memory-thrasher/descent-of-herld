d7c4c36805b2b52de3207113db929f64facb8015
Sidney Kelley <memory.thrasher@gmail.com>
2025-02-03 19:32:16 -0700

    ux panel and layout WIP

---
+++ src/uxPanel.hpp
@@ -0,0 +1,40 @@
+/*
+Copyright 2024-2025 Wafflecat Games, LLC
+
+This file is part of The Descent of Herld.
+
+The Descent of Herld is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+
+The Descent of Herld is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with The Descent of Herld. If not, see <https://www.gnu.org/licenses/>.
+
+Stable and intermediate releases may be made continually. For this reason, a year range is used in the above copyrihgt declaration. I intend to keep the "working copy" publicly visible, even if it is not functional. I consider every push to this publicly visible repository as a release. Releases intended to be stable will be marked as such via git tag or similar feature.
+*/
+
+#pragma once
+
+#include <vector>
+
+namespace doh {
+
+  struct uxLayout;
+
+  struct uxPanel : public uxBase {
+    uxLayout* layout;
+    glm::vec4 bounds;
+    std::vector<uxBase*> children;
+    uxPanel();
+    uxPanel(const uxPanel&) = delete;
+    virtual ~uxPanel() = default;
+    void setLayout(uxLayout*);
+    void redraw();
+    void clear();
+    void push(uxBase*);
+    virtual void update() override;
+    virtual const glm::vec4& getBounds() const override;
+    virtual void setBounds(const glm::vec4&) override;
+    virtual void updateVisible(bool parentVisible) override;
+  };
+
+}
